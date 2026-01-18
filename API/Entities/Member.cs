c2e32b22a96e89b8a25b50663c4f680a678983ec
Jonathan <jojodonghia2005@gmail.com>
2026-01-18 23:23:46 +0100

    end of section 8

---
+++ API/Entities/Member.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace API.Entities;
+
+public class Member
+{
+    public string Id { get; set; }=null!;
+    public DateOnly DateOfBirth { get; set; }
+    public string? ImageUrl { get; set; }
+    public required string DisplayName { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; } 
+    public required string Gender { get; set; }
+    public string? Description { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+
+    
+    //NAVIGATION PROPERTY
+    [JsonIgnore]
+    public List<Photo> photos {get; set;} =[];
+    
+    [JsonIgnore]
+    [ForeignKey(nameof(Id))]
+    public AppUser user { get; set; } =null!;
+    
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. Se vogliamo che siano iniettabili, dobbiamo dichiararli come Services
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
@@ -21,6 +21,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
     //Scoped significa che il servizio viene creato una volta per richiesta http
     //transient significa che viene creato ogni volta che viene richiesto
     //singleton significa che viene creato una volta sola per tutta l'applicazione
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -44,4 +45,20 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 app.UseAuthentication(); //prima autenticazione poi autorizzazione
 app.UseAuthorization();
 app.MapControllers();
+
+//andiamo ad automatizzare migration e se non ci sono utenti andiamo a fare seeding
+using var scope= app.Services.CreateScope(); //createScope serve per creare un ambito per i servizi con scoped lifetime
+var services= scope.ServiceProvider; //get the service provider from the scope
+try
+{
+    var context= services.GetRequiredService<AppDbContext>(); //get the dbcontext from the service provider
+    await context.Database.MigrateAsync(); //apply any pending migrations
+    await Seed.SeedUser(context);
+}
+catch (Exception ex)
+{
+    var logger= services.GetRequiredService<ILogger<Program>>(); //get the logger from the service provider
+    logger.LogError(ex, "an error occured during migration");
+}
+
 app.Run();
